0ab34a2ace21bba62139e604344c93a04fb713da
YacineKouhli <yacine.kouhli@gmail.com>
2023-05-14 02:22:26 +0200

    Added visualisation of the pdf

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, jsonify, render_template
 from .services.pdf_service import PDFService
 from .services.ai_service import AIService
+import PyPDF2
+import base64  # New import for encoding the PDF content
+import io  # New import for handling byte streams
 
 bp = Blueprint('main', __name__)
 pdf_service = PDFService()
@@ -13,13 +16,26 @@ def index():
 @bp.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
-        return 'No file part'
+        return jsonify(error='No file part'), 400
     file = request.files['file']
     if file.filename == '':
-        return 'No selected file'
+        return jsonify(error='No selected file'), 400
     if file:
-        document = pdf_service.extract_text(file)
+        pdf_stream = file.stream.read()  # Read the PDF stream
+        pdf_reader = PyPDF2.PdfReader(io.BytesIO(pdf_stream))  # PyPDF2 now requires PdfReader
+        document = ""
+        for page in pdf_reader.pages:
+            document += page.extract_text()
         question = request.form.get('question')
         answer = ai_service.ask(question, document)
-        return render_template('result.html', answer=answer)
+
+        # Convert the PDF stream to base64
+        pdf_content = base64.b64encode(pdf_stream).decode('utf-8')
+
+        # Return a JSON response with the answer and the PDF content
+        return jsonify(answer=answer, pdf_content=pdf_content)
+
+
+
+
 
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,42 +1,76 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("#uploadForm");
-  const answerDiv = document.getElementById("answer");
-  const spinner = document.getElementById("spinner");
+    const form = document.querySelector("#uploadForm");
+    const answerDiv = document.getElementById("answer");
+    const spinner = document.getElementById("spinner");
+    const pdfContainer = document.getElementById("pdf-container");
 
-  form.addEventListener("submit", function (event) {
-      event.preventDefault();
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
 
-      // Validate inputs
-      const file = form.elements.file.files[0];
-      const question = form.elements.question.value;
-      if (!file || !question) {
-          alert('Please upload a PDF and enter a question.');
-          return;
-      }
+        // Validate inputs
+        const file = form.elements.file.files[0];
+        const question = form.elements.question.value;
+        if (!file || !question) {
+            alert('Please upload a PDF and enter a question.');
+            return;
+        }
 
-      const formData = new FormData(form);
-      spinner.style.display = "block";  // Show the spinner
+        const formData = new FormData(form);
+        spinner.style.display = "block";  // Show the spinner
 
-      fetch("/upload", {
-          method: "POST",
-          body: formData,
-      })
-          .then((response) => {
-              if (!response.ok) {
-                  throw new Error('Network response was not ok');
-              }
-              return response.text();
-          })
-          .then((answer) => {
-              spinner.style.display = "none";  // Hide the spinner
-              answerDiv.innerHTML = answer;
-          })
-          .catch((error) => {
-              spinner.style.display = "none";  // Hide the spinner
-              console.error("Error:", error);
-              alert("Something went wrong. Please try again.");
-          });
-  });
+        fetch("/upload", {
+            method: "POST",
+            body: formData,
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                spinner.style.display = "none";  // Hide the spinner
+                answerDiv.textContent = data.answer;
+
+                // Retrieve the PDF content from the response
+                const pdfContent = data.pdf_content;
+
+                // Render the PDF using the PDF.js viewer
+                const pdfViewer = document.getElementById("pdf-container");
+
+                pdfjsLib.getDocument({ data: atob(pdfContent) }).promise
+                    .then((pdf) => {
+                        pdf.getPage(1).then((page) => {
+                            const scale = 1.5;
+                            const viewport = page.getViewport({ scale });
+                            const canvas = document.createElement("canvas");
+                            const context = canvas.getContext("2d");
+                            canvas.height = viewport.height;
+                            canvas.width = viewport.width;
+                            page.render({
+                                canvasContext: context,
+                                viewport: viewport,
+                            });
+                            pdfViewer.innerHTML = "";
+                            pdfViewer.appendChild(canvas);
+                            pdfContainer.style.display = "block";  // Show the PDF container
+                        });
+                    })
+                    .catch((error) => {
+                        console.error("Error rendering PDF:", error);
+                        alert("Something went wrong. Please try again.");
+                    });
+            })
+            .catch((error) => {
+                spinner.style.display = "none";  // Hide the spinner
+                console.error("Error:", error);
+                alert("Something went wrong. Please try again.");
+            });
+    });
 });
+
+
+  
+
 
   
